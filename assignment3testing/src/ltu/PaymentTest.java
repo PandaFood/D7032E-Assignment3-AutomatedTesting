5d6de125f14a13b77b9448abec887063b36261f2
97gushan <97gustavh@gmail.com>
2019-10-04 13:09:14 +0200

    refactor

--- assignment3testing/src/ltu/PaymentTest.java
+++ assignment3testing/src/ltu/PaymentTest.java
@@ -15,8 +15,14 @@ public class PaymentTest
 {
     PaymentImpl payment;
 
-    static int fullLoan = 2816+7088;
-    static int halfLoan = 1396+3564;
+    static int fullLoan = 7088;
+    static int halfLoan = 3564;
+
+    static int fullSub = 2816;
+    static int halfSub = 1396;
+
+    static int full = fullSub + fullLoan;
+    static int half = halfSub + halfLoan;
 
     @Before
     public void initTest() throws IOException {
@@ -54,35 +60,50 @@ public class PaymentTest
     public void testStudyPace(){
         // [201]
         assertEquals(0, payment.getMonthlyAmount("19900718-0000", 0, 25, 100));
-        assertEquals(halfLoan,payment.getMonthlyAmount("19900718-0000", 0, 50, 100));
 
+    }
+
+    @Test
+    public void testStudyPace50(){
+        assertEquals(half,payment.getMonthlyAmount("19900718-0000", 0, 50, 100));
+    }
+
+    @Test
+    public void testStudyPace75(){
         // [202]
-        assertEquals(halfLoan, payment.getMonthlyAmount("19900718-0000", 0, 75, 100));
+        assertEquals(half, payment.getMonthlyAmount("19900718-0000", 0, 75, 100));
+    }
 
+    @Test
+    public void testStudyPace100(){
         // [203]
-        assertEquals(fullLoan, payment.getMonthlyAmount("19900718-0000", 0, 100, 100));
+        assertEquals(full, payment.getMonthlyAmount("19900718-0000", 0, 100, 100));
     }
 
 
     @Test
-    public void testIncome(){
+    public void testIncomeFull(){
         // [301]
-        assertEquals(fullLoan, payment.getMonthlyAmount("19900718-0000", 0, 100, 100));
-        assertEquals(fullLoan, payment.getMonthlyAmount("19900718-0000", 85813, 100, 100));
+        assertEquals(full, payment.getMonthlyAmount("19900718-0000", 0, 100, 100));
+        assertEquals(full, payment.getMonthlyAmount("19900718-0000", 85813, 100, 100));
         assertEquals(0, payment.getMonthlyAmount("19900718-0000", 100000, 100, 100));
 
-        // [302]
-        assertEquals(halfLoan, payment.getMonthlyAmount("19900718-0000", 0, 75, 100));
-        assertEquals(halfLoan, payment.getMonthlyAmount("19900718-0000", 128722, 75, 100));
-        assertEquals(0, payment.getMonthlyAmount("19900718-0000", 130000, 75, 100));
+
     }
 
+    @Test
+    public void testIncomeHalf(){
+        // [302]
+        assertEquals(half, payment.getMonthlyAmount("19900718-0000", 0, 75, 100));
+        assertEquals(half, payment.getMonthlyAmount("19900718-0000", 128722, 75, 100));
+        assertEquals(0, payment.getMonthlyAmount("19900718-0000", 130000, 75, 100));
+    }
 
     @Test
     public void testCompletionRatio(){
         // [401]
-        assertEquals(fullLoan, payment.getMonthlyAmount("19900718-0000", 0, 100, 100));
-        assertEquals(fullLoan, payment.getMonthlyAmount("19900718-0000", 0, 100, 50));
+        assertEquals(full, payment.getMonthlyAmount("19900718-0000", 0, 100, 100));
+        assertEquals(full, payment.getMonthlyAmount("19900718-0000", 0, 100, 50));
         assertEquals(0, payment.getMonthlyAmount("19900718-0000", 0, 100, 25));
     }
 
@@ -98,10 +119,51 @@ public class PaymentTest
 
 
     @Test
-    public void testAge(){
-        assertEquals(fullLoan, payment.getMonthlyAmount("19900718-0000", 0, 100, 100));
+    public void testAgeLowLimit() {
+        // [101]
         assertEquals(0, payment.getMonthlyAmount("20130718-0000", 0, 100, 100));
-        assertEquals(0, payment.getMonthlyAmount("19200718-0000", 0, 100, 100));
+        assertEquals(full, payment.getMonthlyAmount("19960418-0000", 0, 100, 100));
+
+    }
+    @Test
+    public void testAgeUpperLimit(){
+        // [102]
+        assertEquals(fullSub, payment.getMonthlyAmount("19620718-0000", 0, 100, 100));
+        assertEquals(0, payment.getMonthlyAmount("19590418-0000", 0, 100, 100));
+    }
+
+    @Test
+    public void testAgeLoan(){
+        // [103]
+        assertEquals(fullSub, payment.getMonthlyAmount("19620718-0000", 0, 100, 100) );
+        assertEquals(fullSub, payment.getMonthlyAmount("19690418-0000", 0, 100, 100));
+    }
+
+    @Test
+    public void testFullLoan(){
+        // [501]
+        assertEquals(fullLoan, payment.getMonthlyAmount("19900718-0000", 0, 100, 100) - fullSub);
+    }
+
+    @Test
+    public void testFullSub(){
+        // [502]
+        assertEquals(fullSub, payment.getMonthlyAmount("19900718-0000", 0, 100, 100) - fullLoan);
+    }
+
+    @Test
+    public void testHalfLoan(){
+        // [503]
+        assertEquals(halfLoan, payment.getMonthlyAmount("19900718-0000", 0, 50, 100) - halfSub);
+        assertEquals(halfLoan, payment.getMonthlyAmount("19900718-0000", 0, 99, 100) - halfSub);
+    }
+
+    @Test
+    public void testHalfSub(){
+        // [504]
+        assertEquals(halfSub, payment.getMonthlyAmount("19900718-0000", 0, 75, 100) - halfLoan);
+        assertEquals(halfSub, payment.getMonthlyAmount("19900718-0000", 0, 99, 100) - halfLoan);
+
     }
 }
 
